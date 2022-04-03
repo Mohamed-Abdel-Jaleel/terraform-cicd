ae3e97969a8c6dcdf4a23976d469430d12767949
Mohamed Abdel Jaleel <83928344+Mohamed-Abdel-Jaleel@users.noreply.github.com>
2022-04-03 13:53:03 +0200

    try

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "${ secrets.AWS_ACCESS_KEY_ID }"
-    secret_key = "${ secrets.AWS_SECRET_ACCESS_KEY }"
+    access_key = "AKIAS47TN4SPEO7DMQZ7"
+    secret_key = "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"
 }

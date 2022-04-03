d96d4cfd8e2747b46a9e60ffeec9b9b7bcdb0d44
Mohamed Abdel Jaleel <83928344+Mohamed-Abdel-Jaleel@users.noreply.github.com>
2022-04-03 13:56:38 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAS47TN4SPEO7DMQZ7"
-    secret_key = "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"
+    access_key = "
+    secret_key = "
 }
